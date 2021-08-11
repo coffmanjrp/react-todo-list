69f91c53095ec89c98800c3db1a93a20ff63e982
coffmanjrp <coffmanjrp@gmail.com>
2021-08-10 20:10:58 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Todos</h1>
+      <form className="form">
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter your todo"
+          autocomplete="off"
+        />
+        <ul className="todos"></ul>
+      </form>
+      <small>
+        Left click to toggle completed. <br />
+        Right click to delete todo.
+      </small>
     </div>
   );
 }
